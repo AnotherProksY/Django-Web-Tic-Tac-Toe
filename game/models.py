f69bc1fad77332ebae40da7e95ab2b6caf09a54d
AnotherProksY <k.fazilov@icloud.com>
2020-03-10 15:41:16 +0300

    1. Изменил конфиг CircleCI 2. Добавил .flake8 файл 3. Запустил pipenv run flake8 --statistics и сделал рефакторинг

--- game/models.py
+++ game/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class scoreboard(models.Model):
     human_wins = models.IntegerField()
     computer_wins = models.IntegerField()
--- game/views.py
+++ game/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, "index.html", locals())
 
+
 def play(request):
     return render(request, "play.html", locals())
+
 
 def score(request):
     return render(request, "score.html", locals())
